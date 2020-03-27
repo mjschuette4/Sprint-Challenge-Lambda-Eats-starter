97989165e2a88561ba17af653886d8762ce43a37
MatthewSchuette <mjschuette4@gmail.com>
2020-03-27 12:20:48 -0500

    got Home.js and App.js sorted out and have code for Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import {Route, Link, Switch, BrowserRouter as Router} from "react-router-dom";
+import Form from "./components/Form";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <nav className="navbar">
+          <Link to="/">
+            <button name = 'homebutton'>Home</button>
+            </Link>
+          <br/>
+          <Link to="/form"> 
+          <button name = 'orderbutton'>Order</button>
+          </Link>
+      </nav>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/form" component={Form} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+function Home() {
+    return (
+      <div className="home">
+
+        <h1>Isaac's Pizzaria</h1>
+
+      </div>
+    );
+  }
+
+  export default Home; 
